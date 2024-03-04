dbfad3fd864e9bc583956fd536d7347d4b18d348
sftdevmsh <sft.dev.msh@gmail.com>
2024-03-03 16:50:00 -0800

    to login

--- app/src/main/java/msh/myonlineshop/ErrorActivity.java
+++ app/src/main/java/msh/myonlineshop/ErrorActivity.java
@@ -2,13 +2,32 @@ package msh.myonlineshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ErrorActivity extends AppCompatActivity {
+
+    TextView txtMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_error);
+
+        init();
+    }
+
+    void init(){
+        bindViews();
+
+        Intent intentData = getIntent();
+        String msg = intentData.getStringExtra("msg");
+        txtMsg.setText(msg);
+    }
+
+    void bindViews()
+    {
+        txtMsg = findViewById(R.id.txtMsg);
     }
 }
